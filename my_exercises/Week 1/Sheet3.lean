4aa4a3b2aaa94af79bcbd41ab1e9845a7d0a78e3
nnhjy <43530784+nnhjy@users.noreply.github.com>
2025-09-25 00:10:53 +0200

    add contents to week 1

--- my_exercises/Week 1/Sheet3.lean
+++ my_exercises/Week 1/Sheet3.lean
@@ -22,9 +22,10 @@ def f := fun x : ℕ ↦ fun y : ℕ  ↦ x = y
 example : f 0 0 := by rfl
 
 /-! New tactics
-* `rewrite` [h] - replace a term *in the goal* with an equivalent term [h].
+* `rewrite [h]`   - replace a term in a *goal* with an equivalent term (*hypothesis*: statement, function) `h`.
+* `rewrite [← h]` - replace a term in a *goal* with `h` in the reverse direction stated by `h`.
 * `assumption` - we are done because ∃`h` s.t. `exact h` can close the *goal*
-* `rw` -- rewrite, followed by trying to close the *goal* by rfl.
+* `rw` -- rewrite, followed by trying to close the *goal* by `rfl`.
 -/
 
 example (x: ℕ): f 0 x → x = 0 := by
@@ -34,18 +35,38 @@ example (x: ℕ): f 0 x → x = 0 := by
   assumption
 
 -- Give a direct proof
-example (x: ℕ): f x 1 → x ≠ 2 := by sorry -- [TODO]
+example (x: ℕ): f x 1 → x ≠ 2 := by
+  intro fx
+  rw [f] at fx
+  rw [fx]
+  trivial
 
-example (x y: ℕ): f 0 x ∧ f 0 y → x = y := by sorry --[TODO]
-
+example (x y: ℕ): f 0 x ∧ f 0 y → x = y := by
+  intro af
+  obtain ⟨ fx, fy ⟩ := af
+  rw [f] at fx fy
+  rw [← fx,fy]
 
 /-! Bonus:
 * `by_contra` - assume the negation of the *goal* and prove False
 * `contradiction` - we are done because we have a proof of `h : P` and `h' : ¬ P`
+* `contrapose!` - Transforms the *goal* into its contrapositive and uses pushes negations inside P and Q. Usage matches `contrapose`.
 * `trivial` - apply `rfl` or `assumption` or `contradiction` tactics
 -/
 
 -- Prove by contradiction
-example (h1: a = b): a = b:= by sorry
+example (h1: a = b): a = b := by
+  by_contra neq
+  contradiction
 
-example (x: ℕ): f x 1 → x ≠ 2 := by sorry
+example (x: ℕ): f x 1 → x ≠ 2 := by
+  -- rewrite [f]
+  -- intro x1
+  -- by_contra x2
+  -- rw [x2] at x1
+  -- contradiction
+
+  contrapose!
+  intro x2
+  rw [f, x2]
+  trivial
